9486640ee683f5594d4495df01533287fe2325c0
Surabhi Chaudhari <sc2263@njit.edu>
2018-11-01 16:27:44 -0400

    Task : Created new model named car

---
+++ app/Http/Controllers/CarController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Car;
+use Illuminate\Http\Request;
+
+class CarController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Car  $carController
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Car $carController)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Car  $carController
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Car $carController)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Car  $carController
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Car $carController)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Car  $carController
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Car $carController)
+    {
+        //
+    }
+}
