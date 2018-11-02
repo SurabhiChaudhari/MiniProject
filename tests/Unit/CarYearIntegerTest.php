eb792109ffc2786183dffc7d25d1e7a8ab3f49c6
Surabhi Chaudhari <sc2263@njit.edu>
2018-11-02 12:57:16 -0400

    Task (Test 13): Unit Test 9 - Tested if car year is int datatype

---
+++ tests/Unit/CarYearIntegerTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\car;
+
+class CarYearIntegerTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testCarYearInteger()
+    {
+        $car = car::inRandomOrder()->first();
+        $caryear = (int)$car->Year;
+        $this->assertInternalType("int", $caryear);
+    }
+}
