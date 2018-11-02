a506c8e1e7ee46c31786d42aef4c1c4484e78081
Surabhi Chaudhari <sc2263@njit.edu>
2018-11-01 22:16:27 -0400

    Fix:About PAge fix

--- resources/views/Pages/about.blade.php
+++ resources/views/Pages/about.blade.php
@@ -1,4 +1,20 @@
 @extends('layouts.app')
+
 @section('content')
-    <h1>This is my about page</h1>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">This is a about Page</div>
+
+                    <div class="card-body">
+
+
+                     Welcome to the about page
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
+
