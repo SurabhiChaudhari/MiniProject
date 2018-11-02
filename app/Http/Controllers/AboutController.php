9a11b1f70082d2f0fdb8b444a141fb51f3f19a55
Surabhi Chaudhari <sc2263@njit.edu>
2018-11-01 20:44:09 -0400

    Task : Created Controller and Route for about page

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+class AboutController extends Controller
+{
+    function about(){
+        return view('pages.about');
+    }
+}
