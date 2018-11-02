a484d78214054ab197113a662d2bb15339120719
Surabhi Chaudhari <sc2263@njit.edu>
2018-11-02 02:06:44 -0400

    Task (Test 2):Feature Test 1:check if the login pages return a 200 status code

--- tests/Feature/RegisterTest.php
+++ tests/Feature/RegisterTest.php
@@ -17,4 +17,9 @@ class RegisterTest extends TestCase
         $response = $this->get('/register');
         $response->assertStatus(200);
     }
+    public function testloginPage()
+    {
+        $response = $this->get('/login');
+        $response->assertStatus(200);
+    }
 }
